43efc9816f07b2d148526a4807406eecefa727c3
Cameron Calpin <cameron.calpin.14@cnu.edu>
2017-05-23 20:10:41 -0400

    Added another parameter to table (message)

--- Python - learning/Database Handling/database_logging.py
+++ Python - learning/Database Handling/database_logging.py
@@ -1,37 +1,42 @@
-import sqlite3, os
+import sqlite3, os, sys
 
 class database_log:
-	def __init__(self, file_name):
-		self.file_name = file_name
 	def write(self):
 		db = sqlite3.connect('log_message.db')
-		file_name = input('File Name: ')
-		db.execute('create table files (fileName text)')
-		db.execute('insert into files (fileName text) values (%s)', file_name)
+		cur = db.cursor()
 
 		choice = input('Would you like to write a message? [y/n]')
 		if choice == 'y':
-			db.execute('create table message (msg text)')
-			Message = input('Type your message:\n')
-			db.execute('insert into message (msg text) values (%s text)', Message)
+			cur.execute('DROP TABLE IF EXISTS files')
+			message = input('Enter message: ')
+			cur.execute('CREATE TABLE files (fileName TEXT, msg TEXT)')
+			cur.execute('INSERT INTO files (fileName, msg) VALUES (?, ?)', (str(file_name), (str(message))))
+			db.commit()
 		else:
-			sys.exit()
+			cur.execute('DROP TABLE IF EXISTS files')
+			message = ''
+			cur.execute('CREATE TABLE files (fileName TEXT, msg TEXT)')
+			cur.execute('INSERT INTO files (fileName, msg) VALUES (?, ?)', (str(file_name), (str(message))))
+			db.commit()
 
 	def read(self):
 		db = sqlite3.connect('log_message.db')
-		Files = db.execute('select * from files')
+		cur = db.cursor()
+		
+		Files = cur.execute('SELECT * FROM files')
 		for each in Files:
 			print(each)
 		choose_file = input('What file do you want to read?\n')
 		db.execute('select ')
 
 log = database_log()
+
 while True:
-	choice = input('Hello, do you want:\n a) new file\n b) read file c) Exit')
+	choice = input('Hello, do you want:\n a) new file\n b) read file\n c) Exit')
 	if choice == 'a':
+		file_name = input('File Name: ')
 		log.write()
 	elif choice == 'b':
-		File = insert('What file do you want to read?')
-		
+		log.read()
 	elif choice == 'c':
 		sys.exit()
