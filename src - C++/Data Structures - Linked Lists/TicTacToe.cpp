ab4c96e4670cb5f5d668ed8a4062d00990d21b7e
Cameron Calpin <cameron.calpin.14@cnu.edu>
2017-08-28 22:51:00 -0400

    Boolean logic of winning now keeps track of both players. Tied score is now recognized in while loop. Arithmetic in for-loops runs smoothly.

--- src - C++/Data Structures - Linked Lists/TicTacToe.cpp
+++ src - C++/Data Structures - Linked Lists/TicTacToe.cpp
@@ -1,6 +1,19 @@
+/*	Author: Cameron Calpin
+ *
+ *	Simple 2-player TicTacToe game. Looks sloppy and 
+ *	uses unnecessary resources here and there, but I
+ *	will update and revise the code to be more space 
+ *	efficient.
+ *
+ *	Date: 8/28/17
+ *	version: 1
+ */
+
 #include <iostream>
 #include <stdio.h>
 #include <cstdlib>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -16,14 +29,19 @@ struct node {
 void createBoard(node** board, int value);
 void displayPlayBoard(node* board);
 void displayRefBoard(node* board);
-void p1_move(node** board, node** player, node* curr);
-//void p2_move(node** board, node** p2_pos, node* curr);
-//bool checkWinner(node* board, bool &win);
-void addP1(node** player, int num);
+void p1_move(node** board, node** player, int &count1);
+void p2_move(node** board, node** p2_pos, int &count2);
+void addP1(node** player, int num, int &count1);
+void addP2(node** player, int num, int &count2);
+void possibleWin(node* user, bool &win);
+void checkWinner(node* user, bool &win, string combo);
+void displayP1List(node* p1);
 
 int main() {
-	bool win = false;
+	bool Win = false;
 	int num;
+	int p1_count = 0;
+	int p2_count = 0;
 	node* Board = NULL;
 	node* head = Board;
 	node* p1_pos = NULL;
@@ -32,31 +50,40 @@ int main() {
 	for (int i = 9; i > 0; i--) {
 		createBoard(&Board, i);
 	}
-	while (true) {
+	while (Board != NULL) {
+		// Erase system command to see debugging
 		//system("cls");
 		displayRefBoard(Board);
 		cout << "\n\n";
 		displayPlayBoard(Board);
-		cout << "Enter number: ";
-		cin >> num;
-		addP1(&p1_pos, num);
-		p1_move(&Board, &p1_pos, head);
-		while (p1_pos != NULL) {
-			cout << "P1 list: " << p1_pos->cell_num << " " << endl;
-			p1_pos = p1_pos->next;
-		}	
-		//win = checkWinner(p1_pos, win);
+		displayP1List(p1_pos);
+		p1_move(&Board, &p1_pos, p1_count);
+		possibleWin(p1_pos, Win);
+		if (p1_count == 5) {
+			cout << "It's a Tie!!" << endl;
+			break;
+		}
+		if (Win == true) {
+			cout << "Player 1 Wins!!" << endl;
+			break;
+		}
+		// Debug: prints the bool value if there is a winner
+		//cout << "\n\nGAME STATUS: " << Win << endl;
 
+		// Erase system command to see debugging
 		//system("cls");
 		displayRefBoard(Board);
 		cout << "\n\n";
 		displayPlayBoard(Board);
-		// p2_move(&Board, &p2_pos, head);
-		// while (p2_pos != NULL) {
-		// 	cout << "P2 list: " << p2_pos->cell_num << " " << endl;
-		// 	p2_pos = p2_pos->next;
-		// }
-		//win = checkWinner(p2_pos, win);
+		displayP1List(p2_pos);
+		p2_move(&Board, &p2_pos, p2_count);
+		possibleWin(p2_pos, Win);
+		if (Win == true) {
+			cout << "Player 2 Wins!!" << endl;
+			break;
+		}
+		// Debug: prints the bool value if there is a winner
+		// cout << "\n\nGAME STATUS: " << Win << endl;
 	}
 }
 
@@ -68,6 +95,9 @@ void createBoard(node** board, int value) {
 	*board = cell;
 }
 
+/*
+ *	Displays playing grid
+ */
 void displayPlayBoard(node* board) {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
@@ -78,6 +108,9 @@ void displayPlayBoard(node* board) {
 	}
 }
 
+/*
+ *	Displays numbered grid as guide
+ */
 void displayRefBoard(node* board) {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
@@ -88,7 +121,7 @@ void displayRefBoard(node* board) {
 	}
 }
 
-void p1_move(node** board, node** player, node* curr) {
+void p1_move(node** board, node** p1_pos, int &count1) {
 	int num;
 	node* temp = *board;
 
@@ -98,56 +131,125 @@ void p1_move(node** board, node** player, node* curr) {
 		if (num == temp->cell_num
 			&& temp->player == '-') {
 			temp->player = PLAYER1;
-
-
-			//break;
+			addP1(p1_pos, num, count1);
 		}
-		// else
-		// 	temp = temp->next;
 	}
-	//temp = curr;
 }
 
-void p2_move(node** board, node** p2_pos, node* curr) {
+void p2_move(node** board, node** p2_pos, int &count2) {
 	int num;
-	curr = *board;
 	node* temp = *board;
 
 	cout << "Player 2, enter number: ";
 	cin >> num;
-	while (temp != NULL) {
+	for (temp = *board; temp != NULL; temp = temp->next) {
 		if (num == temp->cell_num
 			&& temp->player == '-') {
 			temp->player = PLAYER2;
-
-			node* p2 = new node;
-			p2->cell_num = num;
-			p2->next = *p2_pos;
-			*p2_pos = p2;
-			break;
+			addP2(p2_pos, num, count2);
 		}
-		else
-			temp = temp->next;
 	}
-	temp = curr;
 }
 
-void addP1(node** player, int num) {
+void addP1(node** player, int num, int &count1) {
 	node* p1 = new node;
 	p1->cell_num = num;
 	p1->player = '-';
-	p1->next = NULL;
-	if (*player == NULL) {
-		*player = p1;
-	}
-	else {
-		p1->next = *player;
-		*player = p1;
-	}
-	//*player = p1;
-	cout << "next node: " << p1->next << endl;
+	p1->next = *player;
+	*player = p1;
+	count1++;
+	cout << "p1 count: " << count1 << endl;
 }
 
-// bool checkWinner(node* board, bool &win) {
+void addP2(node** player, int num, int &count2) {
+	node* p2 = new node;
+	p2->cell_num = num;
+	p2->player = '-';
+	p2->next = *player;
+	*player = p2;
+	count2++;
+	cout << "p2 count: " << count2 << endl;
+}
 
-// }
+void displayP1List(node* p1) {
+	cout << "P1 List: ";
+	while (p1 != NULL) {
+		cout << p1->cell_num << " ";
+		p1 = p1->next;
+	}
+	cout << "\n\n";
+}
+
+void possibleWin(node* user, bool &win) {
+	string one, two, three, four, five,
+	six, seven, eight;
+	for (int i = 1; i < 9; i++) {
+		switch(i) {
+			case 1:
+				one   = "147";
+				checkWinner(user, win, one);
+				cout << "Win: " << win << endl;
+			case 2:
+				two   = "258";
+				checkWinner(user, win, two);
+				cout << "Win: " << win << endl;
+			case 3:
+				three = "369";
+				checkWinner(user, win, three);
+				cout << "Win: " << win << endl;
+			case 4:
+				four  = "123";
+				checkWinner(user, win, four);
+				cout << "Win: " << win << endl;
+			case 5:
+				five  = "456";
+				checkWinner(user, win, five);
+				cout << "Win: " << win << endl;
+			case 6:
+				six   = "789";
+				checkWinner(user, win, six);
+				cout << "Win: " << win << endl;
+			case 7:
+				seven = "159";
+				checkWinner(user, win, seven);
+				cout << "Win: " << win << endl;
+			case 8:
+				eight = "357";
+				checkWinner(user, win, eight);
+				cout << "Win: " << win << endl;
+			default:
+				cout << "Win: " << win << endl;
+				return;
+		}
+	}
+}
+
+void checkWinner(node* user, bool &win, string combo) {
+	string list = "";
+	ostringstream oss;
+
+	cout << "STRING LIST: ";
+	while (user != NULL) {
+		oss << list << user->cell_num;
+		user = user->next;
+	}
+	cout << oss.str() << endl;
+
+	size_t k = 0;
+	for (size_t i = 0; i < oss.str().size(); ++i) {
+		cout << "we at " << i << " out of " << oss.str().size() - 1 << endl;
+		cout << "Look --> " << oss.str().at(i) << endl;
+		for (size_t j = 0; j < combo.length(); ++j) {
+			if (combo[j] == oss.str().at(i)) {
+				k++;
+				cout << "num of k: " << k << endl;
+				if (k == combo.size()) {
+					cout << "Match!" << endl;
+					win = true;
+					break;
+				}
+			}
+		}
+	}
+}
+
