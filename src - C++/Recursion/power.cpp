837908e92232cebc67cae094b2180a3119b4571a
Cameron Calpin <cameron.calpin.14@cnu.edu>
2017-08-30 12:41:10 -0400

    Example code on Recursion

---
+++ src - C++/Recursion/power.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int pow(int x, int y) {
+	int count = 0;
+	if () {
+
+	}
+}
+
+int main() {
+	int x, y, result;
+	cout << "Enter a number: ";
+	cin >> x;
+	cout << "Enter a power: ";
+	cin >> y;
+	result = pow(x, y);
+	cout << result;
+}
