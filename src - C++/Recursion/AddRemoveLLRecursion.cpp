3da3be81f1ace6cc269d33a197b3ea62152b7088
Cameron Calpin <cameron.calpin.14@cnu.edu>
2017-09-19 15:59:22 -0400

    Added TravisCI

---
+++ src - C++/Recursion/AddRemoveLLRecursion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio>
+
+using namespace std;
+
+void addNode(node** list, int data);
+void removeNode();
+
+struct node {
+	int data;
+	node* next;
+}
+
+int main() {
+
+}
