d53e3b6f433949fadf1d322379ea9c49ac292d47
Cameron Calpin <cameron.calpin.14@cnu.edu>
2017-09-25 15:32:01 -0400

    Adding/Removing node using recursion

--- src - C++/Recursion/RecursiveLinkedList.cpp
+++ src - C++/Recursion/RecursiveLinkedList.cpp
@@ -9,34 +9,50 @@ struct node {
 	node* p_next;
 };
 
-void setElements(node** head, int num) {
-	node* p_node = new node;
-	p_node -> number = num;
-	p_node -> p_next = *head;
-	*head = p_node;
+void insertNode(node* temp, int num) {
+	if (temp -> p_next != NULL) {
+		insertNode(temp -> p_next, num);
+	}
+	else {
+		temp -> p_next = new node;
+		temp -> p_next -> number = num;
+		temp -> p_next -> p_next = NULL;
+	}
+}
+
+node* addNode(node*& temp, int num) {
+	if (temp == NULL) {
+		node* N = new node;
+		N -> number = num;
+		N -> p_next = NULL;
+		temp = N;
+		return N;
+	}
+	else {
+		insertNode(temp, num);
+	}
 }
 
 void removeElement(node* list, int num) {
 	node* p_curr = list;
-	while (list != NULL) {
-		if (num == p_curr -> p_next -> number) {
-			list = p_curr;
-			list -> p_next = list -> p_next -> p_next;
-			break;
-		}
+
+	if (list -> p_next != NULL) {
+		// if (num == p_curr -> p_next -> number) {
+		// 	//list = p_curr;
+		// 	list -> p_next = list -> p_next -> p_next;
+		// }
 		if (num == p_curr -> number) {
 			list -> number = list -> p_next -> number;
 			list -> p_next = list -> p_next -> p_next; 
-			break;
 		}
 		else {
-			p_curr = p_curr -> p_next;
+			removeElement(list -> p_next, num);
 		}
 	}
 }
 
 void printList(node* list) {
-	cout << "Numbers in list: ";
+	cout << "Numbers in list: "; 
 	while (list != NULL) {
 		cout << list -> number << " ";
 		list = list -> p_next;
@@ -45,18 +61,18 @@ void printList(node* list) {
 }
 
 int main() {
-	int num;
+	int num, indexCount = 0;
 	node* list = NULL;
 
-	setElements(&list, 5);
-	setElements(&list, 24);
-	setElements(&list, 94);
-	setElements(&list, 44);
-	setElements(&list, 38);
+	node* one = addNode(list, 5);
+	node* two = addNode(list, 24);
+	node* three = addNode(list, 56);
+	node* four = addNode(list, 52);
 
 	printList(list);
 	
-	removeElement(list, 38);
+	removeElement(list, 5);
+	removeElement(list, 24);
 
 	printList(list);
 	
