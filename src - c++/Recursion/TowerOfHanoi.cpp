c483ea8e6dd552ffbac878a13706779e1909813f
Cameron Calpin <cameron.calpin.14@cnu.edu>
2017-11-12 23:04:01 -0500

    Modified Tower of Hanoi

--- src - c++/Recursion/TowerOfHanoi.cpp
+++ src - c++/Recursion/TowerOfHanoi.cpp
@@ -1,59 +1,24 @@
-#include <cstdlib>
-#include <stdio.h>
 #include <iostream>
-#include <stack>
 
 using namespace std;
 
-void move(int height, stack<int> a, stack<int> b, stack<int> c);
-void display(stack<int> a, stack<int> b, stack<int> c);
+void towers(int, char, char, char);
+
+void towers(int num, char frompeg, char topeg, char auxpeg) {
+    if (num == 1) {
+        cout << "\n Move disk 1 from peg " << frompeg << " to peg " << topeg << endl;
+        return;
+    }
+    towers(num - 1, frompeg, auxpeg, topeg);
+    cout << "\n Move disk " << num << " from peg " << frompeg << " to peg " << topeg << endl;
+    towers(num - 1, auxpeg, topeg, frompeg);
+}
 
 int main() {
-	int size;
-	stack<int> A;
-	stack<int> B;
-	stack<int> C;
-
-	cout << "How many disks in tower?" << endl;
-	cin >> size;
-
-	for (int i = size - 1; i >= 0; i--) {
-		A.push(i);
-	}
-
-	display(A, B, C);
-}
-
-void move(int height, stack<int> a, stack<int> b, stack<int> c) {
-	int a_top = a.top();
-	int b_top = b.top();
-	int c_top = c.top();
-	if (height > 1) {
-		// check stack A
-			// Can a.top() be placed in B or C?
-		// check stack B
-			// Can b.top() be placed in A or C?
-		// check stack C
-			// Can c.top() be placed in B or C?
-	}
-}
-
-void display(stack<int> a, stack<int> b, stack<int> c) {
-	cout << "\nA: ";
-	while (!a.empty()) {
-		cout << a.top();
-		a.pop();
-	}
-
-	cout << "\nB: ";
-	while (!b.empty()) {
-		cout << b.top();
-		b.pop();
-	}
-
-	cout << "\nC: ";
-	while (!c.empty()) {
-		cout << c.top();
-		c.pop();
-	}
+    int num;
+    cout << "Enter the number of disks : ";
+    cin >> num;
+    cout << "The sequence of moves involved in the Tower of Hanoi are :\n"<<endl;
+    towers(num, 'A', 'C', 'B');
+    return 0;
 }
