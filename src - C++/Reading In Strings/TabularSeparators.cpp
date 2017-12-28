aeef6ce7fad8dd40c951691939ee5d6f420c451b
Cameron Calpin <cameron.calpin.14@cnu.edu>
2017-12-28 00:23:10 -0500

    Logic recognizes delimiters in string. Separates with tab for every line submitted by user.

--- src - C++/Reading In Strings/TabularSeparators.cpp
+++ src - C++/Reading In Strings/TabularSeparators.cpp
@@ -1,15 +1,103 @@
 #include <iostream>
-#include <string>
+#include <string.h>
+#include <stdio.h>
+#include <cstdlib>
 
 using namespace std;
 
 int main() {
-	string name, domain_name, domain;
+	char *pch;
+	int choice, size = 0; 
+	string name = ",",
+		   domain_name = "@",
+		   domain = ".";
+	bool b_name = false,
+		 b_domain_name = false,
+		 b_domain = false;
 
-	while (1) {
-		getline(cin, name, '\n');
-		cout << "PRINT" << endl;
+	cout << "How many users being registered? ";
+	cin >> size;
 
-		
+	string *credentials = new string[size + 1];
+
+	for (int i = 0; i < size; i++) {
+		getline(cin, credentials[i], '\n');
 	}
+
+	for (int i = 0; i < size; i++) {
+
+		size_t found = credentials[i].find(name);
+		if (found != string::npos) {
+			b_name = true;
+		}
+
+		found = credentials[i].find(domain_name);
+		if (found != string::npos) {
+			b_domain_name = true;
+		}
+
+		found = credentials[i].find(domain);
+		if (found != string::npos) {
+			b_domain = true;
+		}
+	}
+	
+	cout << "\n\nSeparate" << endl;
+	cout << "---------" << endl;
+
+	if (b_name) {
+		cout << "1) Separate by comma" << endl;
+	}
+	if (b_domain_name) {
+		cout << "2) Separate by @ symbol" << endl;
+	}
+	if (b_domain) {
+		cout << "3) Separate by period" << endl;
+	}
+
+	cout << "\n\n";
+	cin >> choice;
+	system("cls");
+	switch(choice) {
+		case 1:
+			for (int i = 0; i < size; i++) {
+				char char_array[credentials[i].size() + 1];
+				strcpy(char_array, credentials[i].c_str());
+				pch = strtok(char_array, ",");
+				while (pch != NULL) {
+					printf("%s\t", pch);
+					pch = strtok(NULL, ",");
+				}
+				cout << '\n';
+			}
+			break;
+		case 2:
+			for (int i = 0; i < size; i++) {
+				char char_array[credentials[i].size() + 1];
+				strcpy(char_array, credentials[i].c_str());
+				pch = strtok(char_array, "@");
+				while (pch != NULL) {
+					printf("%s\t", pch);
+					pch = strtok(NULL, "@");
+				}
+				cout << '\n';
+			}
+			break; 
+		case 3:
+			for (int i = 0; i < size; i++) {
+				char char_array[credentials[i].size() + 1];
+				strcpy(char_array, credentials[i].c_str());
+				pch = strtok(char_array, ".");
+				while (pch != NULL) {
+					printf("%s\t", pch);
+					pch = strtok(NULL, ".");
+				}
+				cout << '\n';
+			}
+			break;
+	}
+
+	delete[] credentials;
+	return 0;
 }
+
