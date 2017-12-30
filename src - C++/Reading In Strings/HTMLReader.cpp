d6f3e84ab5b8f25794530ca4d0a5e6ae704c950e
Cameron Calpin <cameron.calpin.14@cnu.edu>
2017-12-29 23:31:41 -0500

    Determines if syntax is correct. minor issue with identifying and displaying different tags in body.

--- src - C++/Reading In Strings/HTMLReader.cpp
+++ src - C++/Reading In Strings/HTMLReader.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 bool checkHtmlTags(vector<string> str);
 bool checkHeadTags(vector<string> str);
-bool checkBodyTags(vector<string> str);
+bool checkBodyTags(vector<string> str, int &b_index, int &e_index);
+void readTags(vector<string> str, int b_index, int e_index);
 
 int main() {
 	vector<string> values;
 	string input;
+	int b_index = 0, e_index = 0;
 
 	do {
 		getline(cin, input, '\n');
@@ -23,7 +25,8 @@ int main() {
 		return 0;
 	}
 	bool head_result = checkHeadTags(values);
-	bool body_result = checkBodyTags(values);
+	bool body_result = checkBodyTags(values, b_index, e_index);
+	readTags(values, b_index, e_index);
 	
 	return 0;
 }
@@ -71,38 +74,122 @@ bool checkHeadTags(vector<string> str) {
 	}
 
 	if ((beg_head && end_head)
-		 || (beg_head && !end_head)) {
+		 || (!beg_head && !end_head)) {
 		return true;
 	}
 	return false;
 }
 
-bool checkBodyTags(vector<string> str) {
-	int index = 0;
-	int b_index, e_index;
+bool checkBodyTags(vector<string> str, int &b_index, int &e_index) {
+	int beg_index = 0, end_index = 0;
+	bool beg_body = false;
+	bool end_body = false;
 	for (vector<string>::iterator itr =
 		 str.begin(), end = str.end();
 		 itr != end;
-		 ++itr, index++)
-	{
+		 ++itr)
+	{	
+
 		size_t b_body = (*itr).find("<body>");
 		size_t e_body = (*itr).find("</body>");
 		if (b_body != string::npos) {
-			b_index = (*itr).find("<body>");
+			b_index = beg_index;
+			beg_body = true;
 		}
 
 		if (e_body != string::npos) {
-			e_index = (*itr).find("</body>");
+			e_index = end_index;
+			end_body = true;
+		}
+		beg_index++;
+		end_index++;
+	}
+
+	if ((beg_body && end_body)
+		 || (!beg_body && !end_body)) {
+		return true;
+	}
+	return false;
+}
+
+void readTags(vector<string> str, int b_index, int e_index) {
+	int d_index = 0, pos = 0;
+	for (vector<string>::iterator itr =
+		 str.begin() + b_index + 1, end = str.begin() + e_index;
+		 itr != end;
+		 itr++)
+	{
+		bool b_b, e_b, b_i, e_i, b_a, m_a, e_a = false;
+		string b, it, a;
+		d_index++;
+		cout << d_index << endl;
+
+		size_t beg_b = (*itr).find("<b>");
+		size_t end_b = (*itr).find("</b>");
+		if (beg_b != string::npos) {
+			cout << "beg_b" << endl;
+			b_b = true;
+		}
+		if (end_b != string::npos) {
+			cout << "end_b" << endl;
+			e_b = true;
 		}
 
-		for (vector<string>::iterator n_itr = 
-	 		 str.begin() + (b_index + 1),
-			 end = str.begin() + (e_index - 1);
-			 itr != end;
-			 ++itr) 
-			 {
-			 	cout << *n_itr << endl;
-			 }
+		if (b_b && e_b) {
+			b.resize((*itr).size() - 7);
+			b = (*itr).substr(3, (*itr).size() - 7);
+			// for (size_t i = 3; i < (*itr).size() - 4; i++) {
+			// 	b[i - 3] = (*itr)[i];
+			// }
+			cout << "*" << b << "*" << endl;
+			continue;
+		}
+
+		size_t beg_i = (*itr).find("<i>");
+		size_t end_i = (*itr).find("</i>");
+		if (beg_i != string::npos) {
+			b_i = true;
+		}
+		if (end_i != string::npos) {
+			e_i = true;
+		}
+
+		if (b_i && e_i) {
+			it.resize((*itr).size() - 7);
+			it = (*itr).substr(3, (*itr).size() - 7);
+			// for (size_t i = 3; i < (*itr).size() - 4; i++) {
+			// 	it[i - 3] = (*itr)[i];
+			// }
+			cout << "_" << it << "_" << endl;
+			continue;
+		}
+
+		size_t beg_a = (*itr).find("<a href=");
+		size_t mid_a = (*itr).find_last_of(">");
+		size_t end_a = (*itr).find("</a>");
+
+		if (beg_a != string::npos) {
+			b_a = true;
+		}
+		if (mid_a != string::npos) {
+			while (mid_a != string::npos) {
+				d_index++;
+			}
+			pos = d_index;
+			cout << d_index << " ";
+			m_a = true;
+		}
+		if (end_a != string::npos) {
+			e_a = true;
+		}
+
+		if (b_a && m_a && e_a) {
+			a.resize((*itr).size() - 7);
+			a = (*itr).substr(pos, (*itr).size() - 4);
+			cout << "Link text: " << a << endl;
+			continue;
+		}
+
+		b_b, e_b, b_i, e_i, b_a, m_a, e_a = false;
 	}
-	return true;
 }
