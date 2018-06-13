98940185dbdc544539742f2f6e1ed2d9e9bdb18c
Cameron Calpin <cameron.calpin.14@cnu.edu>
2018-06-13 10:50:46 -0400

    Frameworks and Applications added with files

--- Python - learning/Data Structures/shell_cmds.py
+++ Python - learning/Data Structures/shell_cmds.py
@@ -1,37 +1,53 @@
 import string, sys, os, shutil, cmd
 
 class shell_cmds(cmd.Cmd):
-	def __init__(self):
-		cmd.Cmd.__init__(self)
-		self.prompt = "Shell: "
-		self.cur_dir = os.getcwd()
+	def do_EOF(self, line):
+		sys.exit()
 
 	def help_ls(self):
 		print "List the current directory."
-	def do_ls(self, arg):
-		files = [f for f in os.listdir(self.cur_dir)]
-		for f in files:
-			print f
-		print '\n'
+	def do_ls(self, line):
+		if line == '':
+			dirs = [os.curdir]
+		else:
+			dirs = string.split(line)
+		for dirname in dirs:
+			print 'Listing of %s:' % dirname
+			print string.join(os.listdir(dirname), '\n')
 
 	def help_cd(self):
 		print "Change the directory."
-	def do_cd(self, directory):
-		self.cur_dir = directory
+	def do_cd(self, dirname):
+		if dirname == '':
+			dirname = os.environ['HOME']
+		os.chdir(dirname)
+
+	def do_mkdir(self, dirname):
+		os.mkdir(dirname)
 
 	def help_mv(self):
 		print "Move or rename file."
-	def do_mv(src, dst):
-		print src, dst
-		shutil.move(src, dst)
-
+	def do_mv(self, line):
+		src, dst = string.split(line)
+		os.rename(src, dst)
 
 	def help_cp(self):
 		print "Copy a file or directory."
-	def do_cp(self, src, dest):
-		print "swol"
+	def do_cp(self, line):
+		words = string.split(line)
+		sourcefiles, target = words[:-1], words[-1]
+		print sourcefiles
+		for sourcefile in sourcefiles:
+			shutil.copy(sourcefile, target)
+
+	def do_rm(self, line):
+		map(os.remove, string.split(line))
+
+class DirectoryPrompt:
+	def __repr__(self):
+		return  os.getcwd() + '> '
 		
 
-if __name__ == "__main__":
-	shell = shell_cmds()
-	shell.cmdloop()
+shell = shell_cmds()
+shell.prompt = DirectoryPrompt()
+shell.cmdloop()
