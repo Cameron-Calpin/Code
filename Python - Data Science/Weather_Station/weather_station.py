f614a92df2a3537372fd991b30d3fdabcbd69873
Cameron Calpin <cameron.calpin.14@cnu.edu>
2021-12-01 16:30:44 -0500

    Added Go library for practice.

--- Python - Data Science/Weather_Station/weather_station.py
+++ Python - Data Science/Weather_Station/weather_station.py
@@ -8,20 +8,26 @@ station_data = get(url).json()
 
 lons = [data['weather_stn_long'] for data in station_data['items']]
 lats = [data['weather_stn_lat'] for data in station_data['items']]
-temps = [data['ambient_temp'] for data in station_data['items']]
+temps = [data['ground_temp'] for data in station_data['items']]
 
-cc_lat = 55
-cc_lon = 0
+for i,temp in enumerate(temps):
+	temps[i] = (temp * (9/5)) + 32
+
+for stuff in temps:
+	print("\nYeet: {}".format(stuff))
+
+cc_lat = 30
+cc_lon = -90
 
 my_map = Basemap(projection='merc', lat_0 = cc_lat, lon_0 = cc_lon,
                  resolution = 'h' , area_thresh = 1,
-                 llcrnrlon=cc_lon-15, llcrnrlat=cc_lat-7,
-                 urcrnrlon=cc_lon+5, urcrnrlat=cc_lat+5)
+                 llcrnrlon=cc_lon-29, llcrnrlat=cc_lat-8,
+                 urcrnrlon=cc_lon+26, urcrnrlat=cc_lat+19)
 
 my_map.bluemarble()
 
 for lon, lat, temp in zip(lons, lats, temps):
-	if lon >= cc_lon-15 and lon <= cc_lon+5 and lat >= cc_lat-7 and lat <= cc_lat+5:
+	if lon >= cc_lon-29 and lon <= cc_lon+26 and lat >= cc_lat-8 and lat <= cc_lat+19:
 		x,y = my_map(lon, lat)
 		my_map.plot(x, y, 'o', markersize=10, color=(0,0,1))
 		plt.text(x, y, temp, color = 'w', ha='right',va='bottom')
